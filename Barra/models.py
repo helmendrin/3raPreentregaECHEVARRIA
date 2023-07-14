74e192328bd7a8044cbd31332f9e482b8668b87c
Helen Echevarria <eche.helen@gmail.com>
2023-07-13 21:13:59 -0300

    models agregados

--- Barra/models.py
+++ Barra/models.py
@@ -1,28 +1,22 @@
 from django.db import models
 
-class Mesa(models.Model):
-    numero = models.IntegerField()
-    capacidad = models.IntegerField()
-
-    def __str__(self):
-        return f"Mesa {self.numero}"
-
 class Plato(models.Model):
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    # Otros atributos de la clase Plato
+    # ...
+
+class Menu(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    platos = models.ManyToManyField(Plato)
 
     def __str__(self):
         return self.nombre
 
+class Mesa(models.Model):
+    platos = models.ManyToManyField(Plato, through='Pedido', related_name='mesas')
+    pedido = models.OneToOneField('Pedido', null=True, blank=True, on_delete=models.SET_NULL, related_name='mesa')
+
 class Pedido(models.Model):
     mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE)
     platos = models.ManyToManyField(Plato)
-    creado = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Pedido #{self.id} - Mesa {self.mesa.numero}"
-#
-# 
-# 
-# #
--- Barra/views.py
+++ Barra/views.py
@@ -1,23 +1,22 @@
-from django.shortcuts import render
-from .models import Mesa, Pedido, Plato
+from django.db import models
 
-def mostrar_menu(request):
-    platos = Plato.objects.all()
-    return render(request, 'menu.html', {'platos': platos})
+class Plato(models.Model):
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    # Otros atributos de la clase Plato
+    # ...
 
-def tomar_pedido(request, mesa_id):
-    mesa = Mesa.objects.get(id=mesa_id)
-    platos = Plato.objects.all()
-    
-    if request.method == 'POST':
-        platos_seleccionados = request.POST.getlist('platos')
-        pedido = Pedido.objects.create(mesa=mesa)
-        pedido.platos.set(platos_seleccionados)
-        return render(request, 'confirmacion_pedido.html', {'pedido': pedido})
-    
-    return render(request, 'tomar_pedido.html', {'mesa': mesa, 'platos': platos})
+class Menu(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    platos = models.ManyToManyField(Plato)
 
-def gestionar_mesas(request):
-    mesas = Mesa.objects.all()
-    return render(request, 'gestionar_mesas.html', {'mesas': mesas})
+    def __str__(self):
+        return self.nombre
 
+class Mesa(models.Model):
+    platos = models.ManyToManyField(Plato, through='Pedido', related_name='mesas')
+    pedido = models.OneToOneField('Pedido', null=True, blank=True, on_delete=models.SET_NULL, related_name='mesa')
+
+class Pedido(models.Model):
+    mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE)
+    platos = models.ManyToManyField(Plato)
